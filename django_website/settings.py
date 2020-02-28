d684397b1d226a45892dcfa9753232821e510aad
Chinonso Mogo <mogononso@gmail.com>
2020-02-28 02:29:03 -0330

    updated settings

--- django_website/settings.py
+++ django_website/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ou8(^r-(db-%euekt@xt)c+ct10%y_i+yh9bggl$-#&on5eru7'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['mogononso.herokuapp.com']  # 'mogononso.herokuapp.com'
 
