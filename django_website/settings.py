d07061fa62707e565e8d6080acc046b7c463da0b
Chinonso Mogo <mogononso@gmail.com>
2020-03-01 08:43:50 -0330

    updated static files

--- django_website/settings.py
+++ django_website/settings.py
@@ -124,7 +124,7 @@ EMAIL_BACKEND = "sendgrid_backend.SendgridBackend"
 SENDGRID_SANDBOX_MODE_IN_DEBUG = False
 
 AWS_LOCATION = 'static'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'website/static'),)
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'website/static'),)
 # STATIC_URL = '/staticfile/'
 #
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
@@ -135,6 +135,6 @@ AWS_DEFAULT_ACL = 'public-read'
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
 
-#
+
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
