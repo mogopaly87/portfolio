d72ab05e9bad4432786ccf98a83f996228330c73
Chinonso Mogo <mogononso@gmail.com>
2020-07-11 09:22:50 -0230

    updated db again

--- django_website/settings.py
+++ django_website/settings.py
@@ -84,8 +84,8 @@ WSGI_APPLICATION = 'django_website.wsgi.application'
 #                               PASSWORD=os.environ.get('DB_PASS'), HOST='127.0.0.1', PORT='5432'))
 
 # Production
-DATABASES = dict(default=dict(ENGINE='django.db.backends.postgresql', NAME='portfolio', USER=os.environ.get('DB_USER'),
-                              PASSWORD=os.environ.get('DB_PASS'), HOST='mogononso-1619.postgres.pythonanywhere-services.com', PORT='11619'))
+DATABASES = dict(default=dict(ENGINE='django.db.backends.postgresql', NAME='portfolio', USER=os.getenv('DB_USER'),
+                              PASSWORD=os.getenv('DB_PASS'), HOST='mogononso-1619.postgres.pythonanywhere-services.com', PORT='11619'))
 
 
 # Password validation
