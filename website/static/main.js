5c9da37008f838a28d582b91ccfb5ad48dc240cc
mogopaly87 <mogononso@gmail.com>
2020-12-08 15:13:45 -0330

    more changes

--- website/static/main.js
+++ website/static/main.js
@@ -41,9 +41,10 @@ for (i = 0; i < acc.length; i++) {
 i;
 
 $(document).ready(function(){
-  const bg = document.getElementById("home");
-  window.addEventListener('scroll', function(){
-    bg.style.backgroundSize = 160 - +window.pageYOffset/12+'%';
+  
+  $(".flex-one ul li a").click(function(){
+    $(".flex-one ul li a").removeClass("active-nav")
+    $(this).addClass("active-nav");
   })
 })
 
