e028ea6191a0c4a3049591a3672a6cd8096b23fa
mogopaly87 <mogononso@gmail.com>
2020-12-28 10:54:53 -0330

    copied static files for production

--- static/animation.js
+++ static/animation.js
@@ -22,9 +22,92 @@ $(document).ready(function() {
     .setClassToggle("#exp3", "show")
     .addTo(controller);
 
+  var scene = new ScrollMagic.Scene({
+    triggerElement: "#flexing1",
+    duration: "100%",
+    triggerHook: 0.9
+  })
+    .setClassToggle("#flexing1", "show_app")
+    .addTo(controller);
+
+  var scene = new ScrollMagic.Scene({
+    triggerElement: "#flexing2",
+    duration: "100%",
+    triggerHook: 0.9
+  })
+    .setClassToggle("#flexing2", "show_app")
+    .addTo(controller);
+
+  var scene = new ScrollMagic.Scene({
+    triggerElement: "#flexing3",
+    duration: "100%",
+    triggerHook: 0.9
+  })
+    .setClassToggle("#flexing3", "show_app")
+    .addTo(controller);
 
   $('.scroll').click(function(){
     $('.scroll').removeClass('active-anchor');
     $(this).addClass('active-anchor');
   });
+
+  // Change active side nav on scroll from page to page
+  new ScrollMagic.Scene({
+    triggerElement: "#home",
+    duration: "100%",
+    triggerHook: 0.8
+  })
+    .setClassToggle("#home-nav", "active-nav")
+    .addTo(controller);
+
+  new ScrollMagic.Scene({
+    triggerElement: "#about",
+    duration: "100%",
+    triggerHook: 0.8
+  })
+    .setClassToggle("#about-nav", "active-nav")
+    .addTo(controller);
+
+  new ScrollMagic.Scene({
+    triggerElement: "#skills",
+    duration: "100%",
+    triggerHook: 0.8
+  })
+    .setClassToggle("#skills-nav", "active-nav")
+    .addTo(controller);
+
+  new ScrollMagic.Scene({
+    triggerElement: "#education",
+    duration: "100%",
+    triggerHook: 0.8
+  })
+    .setClassToggle("#education-nav", "active-nav")
+    .addTo(controller);
+
+  new ScrollMagic.Scene({
+    triggerElement: "#portfolio",
+    duration: "100%",
+    triggerHook: 0.8
+  })
+    .setClassToggle("#portfolio-nav", "active-nav")
+    .addTo(controller);
+
+  new ScrollMagic.Scene({
+    triggerElement: "#experience",
+    duration: "100%",
+    triggerHook: 0.8
+  })
+    .setClassToggle("#experience-nav", "active-nav")
+    .addTo(controller);
+
+  new ScrollMagic.Scene({
+    triggerElement: "#contact",
+    duration: "100%",
+    triggerHook: 0.8
+  })
+    .setClassToggle("#contact-nav", "active-nav")
+    .addTo(controller);
+  
+  let triggerPosition = scene.triggerPosition();
+  console.log(triggerPosition);
 });
--- static/main.js
+++ static/main.js
@@ -1,32 +1,3 @@
-function myFunction() {
-  var ebody = document.getElementById("body");
-  var id_one = document.getElementById("id-one");
-  var id_two = document.getElementById("id-two");
-  var id_three = document.getElementById("id-three");
-  var id_four = document.getElementById("id-four");
-  var id_five = document.getElementById("id-five");
-  var id_six = document.getElementById("id-six");
-  var id_seven = document.getElementById("id-seven");
-  var x = ebody.scrollTop;
-  var h = 2 * window.innerHeight;
-  if (x > 1200) {
-    id_one.classList.add("active");
-    id_two.classList.add("active");
-    id_three.classList.add("active");
-    id_four.classList.add("active");
-    id_five.classList.add("active");
-    id_six.classList.add("active");
-    id_seven.classList.add("active");
-  } else {
-    id_one.classList.remove("active");
-    id_two.classList.remove("active");
-    id_three.classList.remove("active");
-    id_four.classList.remove("active");
-    id_five.classList.remove("active");
-    id_six.classList.remove("active");
-    id_seven.classList.remove("active");
-  }
-}
 
 var acc = document.getElementsByClassName("accordion");
 var i;
@@ -39,3 +10,17 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 i;
+
+$(document).ready(function(){
+  
+  $(".flex-one ul li a").click(function(){
+    $(".flex-one ul li a").removeClass("active-nav")
+    $(this).addClass("active-nav");
+  })
+  $(document).scroll(function(){
+    var scr = $("#home").scrollTop()
+    console.log(scr);
+  })
+
+})
+
